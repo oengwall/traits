2e1e6ec7aab74b7c1865df6344e4164bbb8628d3
Ola Engwall <ola.engwall@gmail.com>
2015-07-07 11:47:26 +0200

    Modified setup for personal wheel-building

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from os.path import join
 from setuptools import setup, Extension, find_packages
-
+from setuptools.dist import Distribution
 
 d = {}
 execfile(join('traits', '__init__.py'), d)
@@ -15,6 +15,9 @@ ctraits = Extension(
     extra_compile_args = ['-DNDEBUG=1', '-O3'],
     )
 
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
 
 setup(
     name = 'traits',
@@ -22,22 +25,7 @@ setup(
     url = 'http://code.enthought.com/projects/traits',
     author = 'David C. Morrill, et. al.',
     author_email = 'dmorrill@enthought.com',
-    classifiers = [c.strip() for c in """\
-        Development Status :: 5 - Production/Stable
-        Intended Audience :: Developers
-        Intended Audience :: Science/Research
-        License :: OSI Approved :: BSD License
-        Operating System :: MacOS
-        Operating System :: Microsoft :: Windows
-        Operating System :: OS Independent
-        Operating System :: POSIX
-        Operating System :: Unix
-        Programming Language :: C
-        Programming Language :: Python
-        Topic :: Scientific/Engineering
-        Topic :: Software Development
-        Topic :: Software Development :: Libraries
-        """.splitlines() if len(c.strip()) > 0],
+    classifiers = ['Private :: Do Not Upload'],
     description = 'explicitly typed attributes for Python',
     long_description = open('README.rst').read(),
     download_url = ('http://www.enthought.com/repo/ets/traits-%s.tar.gz' %
@@ -50,4 +38,5 @@ setup(
     packages = find_packages(),
     platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
     zip_safe = False,
+    dist_class= BinaryDistribution
 )
